426f6d40b137a18344527bcd70602dd01424a967
gustycastro <gusty12rd@gmail.com>
2023-06-06 12:24:54 -0300

    desafio1

--- main.js
+++ main.js
@@ -1,56 +1,63 @@
 class ProductManager {
   constructor() {
     this.products = [];
-    this.primerID = 1;
   }
 
-  generarID() {
-    const ID = "Products" + this.primerID;
-    this.primerID++;
-    return ID;
+  #id= 0
+  addProduct = (title, description, price, thumbnail, code, stock) => {
+    this.title = title
+    this.description = description  
+    this.price = price              
+    this.thumbnail = thumbnail      
+    this.code = code                
+    this.stock = stock
+
+    if ( ( this.products.some( p => p.code !== code) ) || this.products.length === 0 ) {
+      if ( !!title && !!description && !!price && !!code && !!thumbnail && !!stock ) {
+          this.#id++
+          this.products.push( { id: this.#id, title, description, price, thumbnail, code, stock } )
+          return "Producto agregado con exitos."
+      } else {
+          return "Todos los campos tienen que ser obligatorios.";
+      }
+  } else {
+      return "¡Dato code repetido! Deberá ingresar otro.";
   }
+}
 
-  addProduct = (title, description, price, thumbnail, stock) => {
-    if (!title || !description || !price || !thumbnail) return "Todos los campos tienen que ser obligatorios."
-    const codeExiste = this.products.some((producto) => producto.code === this.generarID());
-    if (codeExiste) return "El ID del producto ya existe";
-
-    const producto = {
-      title,
-      description,
-      price,
-      thumbnail,
-      stock: stock || 80,
-      code: this.generarID(),
-    };
-
-    this.products.push(producto);
-    return "Producto agregado con exitos.";
-  };
-
-  getProducts = () => {
-    return this.products;
-  };
-
-  getProductsById = (ingresarID) => {
-    let encontrado = this.products.filter((producto) => producto.code === ingresarID);
-    if (encontrado.length > 0) {
-      return encontrado;
-    } else {
-      return "Not found.";
-    }
-  };
+getProducts() {
+  return this.products;
+}
+getProductsById(ID) {
+  let filtro = 0
+  if ( this.products.some( p => p.id === ID) ) {
+      filtro = this.products.find(p => p.id === ID)
+      return filtro
+  } else {
+      return `ID: ${ID} Not found`
+  }
+}
 }
 
 const producto = new ProductManager();
-console.log(producto.addProduct("PC", "AMD Ryzen 8", 250000, "Sin imagen", "15"));
-console.log(producto.addProduct("Mouse", "Corsair Ironclaw RGB", 80000, "Sin imagen", "10")); 
-console.log(producto.addProduct("Monitor", "27¨ MSI Optix G27C4", 50000, "Sin imagen", "20")); 
-console.log(producto.addProduct("Microfono", "NZXT Capsule", 15000, "Sin imagen", "25"));
-console.log(producto.addProduct("Teclado", "", "","", "Sin imagen", "25")); 
 
+//Productos cargados
+console.log("----------------------------------------------");
+console.log(producto.addProduct("PC", "AMD Ryzen 8", 250000, "Sin imagen", "cabj1", 15));
+console.log(producto.addProduct("PC", "AMD Ryzen 8", 250000, "Sin imagen", "cabj1", 15));
+console.log(producto.addProduct("Mouse", "Corsair Ironclaw RGB", 80000, "Sin imagen", "cabj2", 10)); 
+console.log(producto.addProduct("Monitor", "27¨ MSI Optix G27C4", 50000, "Sin imagen", "cabj3", 20)); 
+console.log(producto.addProduct("Microfono", "NZXT Capsule", 15000, "Sin imagen", "cabj4", 25));
+console.log(producto.addProduct("Teclado", "", "","", "Sin imagen", "cabj5", 25));
 
+//Listado de los productos
+console.log("----------------------------------------------");
 console.log("Listado de Productos:", producto.getProducts());
 console.log("----------------------------------------------");
-console.log("Producto encontrado:", producto.getProductsById("Products1"));
-console.log("Producto encontrado:", producto.getProductsById("Products7"));
+
+//ID existente
+console.log("Producto encontrado:", producto.getProductsById(1));
+
+//ID inexistente
+console.log("----------------------------------------------");
+console.log("Producto encontrado:", producto.getProductsById(5));
